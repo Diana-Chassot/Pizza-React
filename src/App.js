dada0af227ac15493e180198d7622ad9e00c759b
Diana-Chassot <diana.chassot25@gmail.com>
2023-06-26 09:34:14 +0200

    path

--- src/App.js
+++ src/App.js
@@ -6,24 +6,24 @@ import Footer from './components/Footer';
 import Contact from './pages/Contact';
 import Cart from './components/dropdowns/Cart';
 import Form from './components/Form';
-import { BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
 
-      <HashRouter basename="/">
-        <Form />
-        <Nav />
-        <Cart />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path="/menu" element={<Menu />} />
-          <Route path="/about" element={<About />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-        <Footer className="footer footer--light" />
-      </HashRouter>
+    <Router basename="/Pizza-React">
+      <Form />
+      <Nav />
+      <Cart />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/about" element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
+      <Footer className="footer footer--light" />
+    </Router>
 
   );
 }
