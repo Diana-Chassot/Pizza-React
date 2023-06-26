7a89530914fea8f331ffd289344b5af4ed0f4f0d
Diana-Chassot <diana.chassot25@gmail.com>
2023-06-26 09:45:49 +0200

    fixed path

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import LogoSrc from "../assets/img/piza-logo.png";
 function Logo() {
   return (
     <div className="logo-container">
-      <Link to={"/Pizza-React"}>
+      <Link to={"/"}>
         <img src={LogoSrc} alt="logo" width="50px" />
         <h2>Italian Pizza</h2>
       </Link>
